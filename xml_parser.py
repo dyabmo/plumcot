2aa43a5e30bc3fec18f26a3ff2efd2dbf4b826d0
dyabmo <dyabmo@github.com>
2017-03-07 17:55:30 +0100

    Initial processing of XGTF file

--- xml_parser.py
+++ xml_parser.py
@@ -1,20 +1,44 @@
+#############################################################################
+#
+# LIMSI - Orsay
+# Equipe TLP
+#
+# Authors:
+# Mohamed Dyab
+#
+#############################################################################
+
+#Argument 1 :
+#Argument 2 :
+
+import sys
 from lxml import etree
 from lxml import objectify
 from io import StringIO, BytesIO
+from xml.dom import minidom
+
+#Usage of xml_parser.py :
+
 
 #TODO
-#Parameterize the filenames
+#Enter filenames as arguments
+#assert arguments assert len(sys.argv) == 3, "Error with number of argument : python extract-keyframe.py <video_path> <nframes>"
 #READ XGTF File
+#Convert from frames to seconds on XGTF
 
-tree = etree.parse("/vol/work1/dyab/BFMTV_CultureEtVous_2012-04-16_065040.trs")
-#print(etree.tostring(tree.getroot()))
+######################################################################
+# Process TRS file
+######################################################################
+
+#Read TRS file
+trs_tree  = etree.parse("/vol/work1/dyab/BFMTV_CultureEtVous_2012-04-16_065040.trs")
 
 # Create new dictionary to add speaker ids as key and speaker names as value
 speaker_id_name_dict = {}
 id_temp,name_temp = "",""
 
 #Parse speaker IDs and names
-for speaker in tree.xpath('//Speakers/Speaker'):
+for speaker in trs_tree.xpath('//Speakers/Speaker'):
     for key,value in speaker.attrib.items():
 
         if(key=="id"):
@@ -26,8 +50,7 @@ for speaker in tree.xpath('//Speakers/Speaker'):
         speaker_id_name_dict[id_temp] = name_temp
 
 #Testing
-print(speaker_id_name_dict["spk1"])
-print(speaker_id_name_dict)
+#print(speaker_id_name_dict)
 
 #Create a list of speech turns
 speech_turn_list = list()
@@ -35,7 +58,7 @@ speaker_id_temp,startTime_temp,endTime_temp = "","",""
 update_list_boolean=False
 
 #Search for <Turn> tags
-for turn in tree.xpath('//Section/Turn'):
+for turn in trs_tree.xpath('//Section/Turn'):
     for key,value in turn.attrib.items():
 
         if (key == "speaker"):
@@ -64,5 +87,48 @@ for turn in tree.xpath('//Section/Turn'):
             speaker_id_temp, startTime_temp, endTime_temp = "", "", ""
 
 #Test
-print(speech_turn_list)
-print(len(speech_turn_list))
+#print(speech_turn_list)
+#print(len(speech_turn_list))
+
+######################################################################
+# Process XGTF file
+######################################################################
+
+#Read XGTF file
+xmldoc = minidom.parse("/vol/work1/dyab/BFMTV_CultureEtVous_2012-04-16_065040.xgtf")
+
+itemlist = xmldoc.getElementsByTagName('object')
+
+for item in itemlist:
+    # Only process objects with name="Personne"
+    if( item.attributes['name'].value == "PERSONNE"):
+        #print(item.attributes["framespan"].value)
+
+        #Get attributes of each Object
+        attribs = item.getElementsByTagName('attribute')
+        for attrib in attribs:
+
+            #Search for attribute NOM to get the name of the face coordinates
+            if (attrib.attributes['name'].value == "NOM"):
+                data_item = attrib.getElementsByTagName("data:svalue")
+                #Search for first data item since only one data tag exists in this <attribute name="NOM"> tag
+                print(data_item[0].attributes["value"].value)
+
+            # Search for attribute TETE
+            if (attrib.attributes['name'].value == "TETE"):
+                print(attrib.attributes["name"].value)
+
+
+
+# process_object_boolean = False
+# #Traverse objects in XGTF file
+# for object_tag in xgtf_tree.xpath('//data'):
+#     print(object_tag)
+#     for key,value in object_tag.attrib.items():
+#         print(value)
+#
+#         #Only process objects with name="Personne"
+#         if (key == "name" and value =="PERSONNE"):
+#             process_object_boolean = True
+#             print(value)
+

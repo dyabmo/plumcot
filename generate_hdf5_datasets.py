5ab74ce4afd523d6063d7777646c56696c39499d
dyabmo <dyabmo@github.com>
2017-05-02 17:30:09 +0200

    Divide training set to training and validation. Add command line arguments.

--- generate_hdf5_datasets.py
+++ generate_hdf5_datasets.py
@@ -1,6 +1,9 @@
 from glob import glob
 import numpy as np
 import h5py
+import random
+import sys
+import os
 
 INPUT_WIDTH=224
 INPUT_HEIGHT=224
@@ -13,6 +16,7 @@ number_of_nodes=12
 training_no_samples=0
 development_no_samples=0
 test_no_samples=0
+SHUFFLE_SAMPLES_INPLACE=False
 
 def validate_dataset_shape(dir):
 
@@ -45,25 +49,85 @@ def validate_dataset_shape(dir):
 
     return validate
 
+def get_file_names(dir):
+
+    file = open(dir, "r")
+    result = file.readlines()
+    names_list = list(map(lambda x : x[0:-1] ,result))
+    file.close()
+
+    return names_list
+
+def get_numpy_arrays(dir,names_list):
+
+    Xv_array=list()
+    Y_array=list()
+
+    for item in names_list:
+        Xv_array.append(glob(dir + "/" + item + "*.Xv.npy"))
+        Y_array.append(glob(dir + "/" + item + "*.Y.npy"))
+
+    #remove empty entries
+    Xv_array=list(filter(lambda x:len(x)!=0 ,Xv_array) )
+    Y_array = list(filter(lambda x: len(x) != 0, Y_array))
+
+    #flatten list
+    flattened_Xv_array = [val for sublist in Xv_array for val in sublist]
+    flattened_Y_array =  [val for sublist in Y_array  for val in sublist]
+
+    return flattened_Xv_array, flattened_Y_array
+
 def save_to_hdf5(dir,output_file,type="training"):
 
-    #Get x
-    x_fnames = glob(dir + "/*.Xv.npy")
-    x_fnames.sort()
-    x_arrays = [np.load(f) for f in x_fnames]
-    x_dataset = np.concatenate(x_arrays)
+    #Set training and validation video names, to be able to add the validation videos in the end
+    validation_list = get_file_names("/vol/work1/dyab/validation_video_list")
+    all_list = get_file_names("/vol/work1/dyab/train_video_list")
 
-    #Get y
-    y_fnames = glob(dir + "/*.Y.npy")
-    y_fnames.sort()
-    y_arrays = [np.load(f) for f in y_fnames]
-    y_dataset = np.concatenate(y_arrays)
+    #Exclude validation file names from list of all names to get training names
+    training_only_list = [val for val in all_list if val not in validation_list]
+
+    #Shuffle training set with respect to videos
+    random.shuffle(training_only_list)
+
+    #get numpy arrays for training and validation
+    train_x_fnames, train_y_fnames = get_numpy_arrays(dir,training_only_list)
+    validation_x_fnames, validation_y_fnames = get_numpy_arrays(dir,validation_list)
+
+    #concatenate training and validation input
+    x_train_arrays=[np.load(f) for f in train_x_fnames]
+    x_train_arrays=np.concatenate(x_train_arrays)
+
+    x_validate_arrays=[np.load(f) for f in validation_x_fnames]
+    x_validate_arrays=np.concatenate(x_validate_arrays)
+
+    x_dataset = np.concatenate((x_train_arrays,x_validate_arrays))
+
+    #concatenate training and validation output
+    y_train_arrays = [np.load(f) for f in train_y_fnames]
+    y_train_arrays = np.concatenate(y_train_arrays)
+
+    y_validate_arrays = [np.load(f) for f in validation_y_fnames]
+    y_validate_arrays = np.concatenate(y_validate_arrays)
+
+    y_dataset = np.concatenate((y_train_arrays, y_validate_arrays))
+
+    print(x_dataset.shape)
+    print(y_dataset.shape)
+
+    if SHUFFLE_SAMPLES_INPLACE:
+        #Shuffle samples inplace: not useful! must shuffle videos themselves
+        index = np.arange(x_dataset.shape[0])
+        np.random.shuffle(index)
+        x_dataset=x_dataset[index]
+        y_dataset = y_dataset[index]
 
     f = h5py.File(output_file, 'w')
 
     if(type == "training"):
 
         f.attrs['train_size'] = x_dataset.shape[0]
+        f.attrs['validation_size'] = x_validate_arrays.shape[0]
+        print(f.attrs['validation_size'])
         # Creating dataset to store features
         f.create_dataset('training_input', data=x_dataset)
 
@@ -93,19 +157,21 @@ def save_to_hdf5(dir,output_file,type="training"):
     f.flush()
     f.close()
 
+def process_arguments():
+
+    assert len(sys.argv) == 4, "Error with number of arguments: <type> <directory> <outputfile name>"
+    assert (sys.argv[1]=="training" or sys.argv[1]=="development" or sys.argv[1]=="test" )
+    assert (os.path.isdir(sys.argv[2])),"Error, directory doesn't exist"
+    assert (not os.path.isfile(sys.argv[3])), "Error: file already exists, can't overwrite it"
+
+    type = sys.argv[1]
+    path = sys.argv[2]
+    outputfile = sys.argv[3]
+
+    return type, path, outputfile
+
 if __name__ == "__main__":
 
-    training_path = "/vol/work1/dyab/training_set/"
-    training_file_name = "/train_dataset2.h5"
-    input_file = training_path + training_file_name
+    type, path, outputfile = process_arguments()
 
-    development_path = "/vol/work1/dyab/development_set/"
-    development_file_name = "/develop_dataset2.h5"
-    development_file = development_path + development_file_name
-
-    test_path = "/vol/work1/dyab/test_set/"
-    test_file_name = "/test_dataset.h5"
-    test_file = test_path + test_file_name
-
-    if(validate_dataset_shape(test_path)):
-        save_to_hdf5(test_path,test_file,type="test")
+    save_to_hdf5(path, outputfile, type=type)

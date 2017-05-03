f5b32957baf45d1bf65038d00c954840c624542d
dyabmo <dyabmo@github.com>
2017-05-03 12:29:40 +0200

    Handle development set. Perform faster validation for daatset shape. Remove unneeded variables.

--- generate_hdf5_datasets.py
+++ generate_hdf5_datasets.py
@@ -9,45 +9,7 @@ INPUT_WIDTH=224
 INPUT_HEIGHT=224
 INPUT_CHANNEL=3
 input_shape=(INPUT_WIDTH,INPUT_HEIGHT,INPUT_CHANNEL)
-DEFAULT_IMAGE_SIZE=224
-nb_epoch = 100
-batch_size=32
-number_of_nodes=12
-training_no_samples=0
-development_no_samples=0
-test_no_samples=0
 SHUFFLE_SAMPLES_INPLACE=False
-
-def validate_dataset_shape(dir):
-
-    validate = True
-
-    #Validate input shape
-    x_fnames = glob(dir + "/*.Xv.npy")
-    x_fnames.sort()
-    x_size=0
-    for f in x_fnames:
-        array = np.load(f)
-        x_size = x_size + array.shape[0]
-
-        if (array.shape[1] != INPUT_WIDTH or array.shape[2] != INPUT_HEIGHT or array.shape[3] != INPUT_CHANNEL):
-            print(f + ":  " + str(array.shape))
-            validate=False
-    print("x_size: " + str(x_size))
-    #Validate output shape
-    y_fnames = glob(dir + "/*.Y.npy")
-    y_fnames.sort()
-    y_size=0
-    for f in y_fnames:
-        array = np.load(f)
-        y_size = y_size + array.shape[0]
-
-    #validate that both are equal in shape
-    if(x_size != y_size):
-        print("x_size: " + str(x_size) + ", y_size: "+str(y_size))
-        validate = False
-
-    return validate
 
 def get_file_names(dir):
 
@@ -79,37 +41,66 @@ def get_numpy_arrays(dir,names_list):
 
 def save_to_hdf5(dir,output_file,type="training"):
 
-    #Set training and validation video names, to be able to add the validation videos in the end
-    validation_list = get_file_names("/vol/work1/dyab/validation_video_list")
-    all_list = get_file_names("/vol/work1/dyab/train_video_list")
+    # validate shape along saving file to save time
+    validate = True
 
-    #Exclude validation file names from list of all names to get training names
-    training_only_list = [val for val in all_list if val not in validation_list]
+    #If type is training, must do a special handling to split to validation set
+    if(type=="training"):
+        #Set training and validation video names, to be able to add the validation videos in the end
+        validation_list = get_file_names("/vol/work1/dyab/validation_video_list")
+        all_list = get_file_names("/vol/work1/dyab/train_video_list")
 
-    #Shuffle training set with respect to videos
-    random.shuffle(training_only_list)
+        #Exclude validation file names from list of all names to get training names
+        training_only_list = [val for val in all_list if val not in validation_list]
 
-    #get numpy arrays for training and validation
-    train_x_fnames, train_y_fnames = get_numpy_arrays(dir,training_only_list)
-    validation_x_fnames, validation_y_fnames = get_numpy_arrays(dir,validation_list)
+        #Shuffle training set with respect to videos
+        random.shuffle(training_only_list)
 
-    #concatenate training and validation input
-    x_train_arrays=[np.load(f) for f in train_x_fnames]
-    x_train_arrays=np.concatenate(x_train_arrays)
+        #get numpy arrays for training and validation
+        train_x_fnames, train_y_fnames = get_numpy_arrays(dir,training_only_list)
+        validation_x_fnames, validation_y_fnames = get_numpy_arrays(dir,validation_list)
 
-    x_validate_arrays=[np.load(f) for f in validation_x_fnames]
-    x_validate_arrays=np.concatenate(x_validate_arrays)
+        #concatenate training and validation input
+        x_train_arrays=[np.load(f) for f in train_x_fnames]
+        x_train_arrays=np.concatenate(x_train_arrays)
 
-    x_dataset = np.concatenate((x_train_arrays,x_validate_arrays))
+        x_validate_arrays=[np.load(f) for f in validation_x_fnames]
+        x_validate_arrays=np.concatenate(x_validate_arrays)
 
-    #concatenate training and validation output
-    y_train_arrays = [np.load(f) for f in train_y_fnames]
-    y_train_arrays = np.concatenate(y_train_arrays)
+        x_dataset = np.concatenate((x_train_arrays,x_validate_arrays))
 
-    y_validate_arrays = [np.load(f) for f in validation_y_fnames]
-    y_validate_arrays = np.concatenate(y_validate_arrays)
+        #concatenate training and validation output
+        y_train_arrays = [np.load(f) for f in train_y_fnames]
+        y_train_arrays = np.concatenate(y_train_arrays)
 
-    y_dataset = np.concatenate((y_train_arrays, y_validate_arrays))
+        y_validate_arrays = [np.load(f) for f in validation_y_fnames]
+        y_validate_arrays = np.concatenate(y_validate_arrays)
+
+        y_dataset = np.concatenate((y_train_arrays, y_validate_arrays))
+
+    #If type is development or test, handle normally
+    elif (type == "development" or type == "test"):
+        # Get x
+        x_fnames = glob(dir + "/*.Xv.npy")
+        x_fnames.sort()
+        x_arrays = [np.load(f) for f in x_fnames]
+        x_dataset = np.concatenate(x_arrays)
+
+        # Get y
+        y_fnames = glob(dir + "/*.Y.npy")
+        y_fnames.sort()
+        y_arrays = [np.load(f) for f in y_fnames]
+        y_dataset = np.concatenate(y_arrays)
+
+    #validate x_dataset shape
+    if (x_dataset.shape[1] != INPUT_WIDTH or x_dataset.shape[2] != INPUT_HEIGHT or x_dataset.shape[3] != INPUT_CHANNEL):
+        validate = False
+        return validate
+
+    if (y_dataset.shape[0] != x_dataset.shape[0]):
+        print("x_dataset size: " + str(x_dataset.shape[0]) + ", y_dataset size: " + str(y_dataset.shape[0]))
+        validate = False
+        return validate
 
     print(x_dataset.shape)
     print(y_dataset.shape)
@@ -156,6 +147,7 @@ def save_to_hdf5(dir,output_file,type="training"):
 
     f.flush()
     f.close()
+    return validate
 
 def process_arguments():
 
@@ -174,4 +166,6 @@ if __name__ == "__main__":
 
     type, path, outputfile = process_arguments()
 
-    save_to_hdf5(path, outputfile, type=type)
+    validate = save_to_hdf5(path, outputfile, type=type)
+    if(not validate):
+        print("Error in dataset shape...")

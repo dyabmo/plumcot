e2fee5077f8dc0fef325cc3cbe1b2fe66e42e7e5
dyabmo <dyabmo@github.com>
2017-07-20 12:17:13 +0200

    Script to extract audio MFCC features for facetracks.

---
+++ audio_features.py
@@ -0,0 +1,64 @@
+from pyannote.core import Segment
+from pyannote.audio.features import Precomputed
+import pandas as pd
+import numpy as np
+from glob import glob
+
+# REPERE evaluation protocol
+# cf. https://github.com/pyannote/pyannote-database#pyannote-database
+from pyannote.database import get_protocol
+#different from the files I use
+protocol = get_protocol('REPERE.SpeakerDiarization.Plumcot')
+precomputed = Precomputed('/vol/work1/dyab/training_set/mfcc')
+
+dev_dir="/vol/work1/dyab/development_set/residual_cluster_old/"
+train_dir="/vol/work1/dyab/training_set/residual_local/"
+test_dir="/vol/work1/dyab/test_set/residual/"
+output_dir_train ="/vol/work1/dyab/training_set/numpy_arrays_local_audio/"
+y_labels_dir = "/vol/work1/dyab/training_set/numpy_arrays_local_landmarks/"
+
+for current_file in protocol.train():      # iterate on all files of Phase2 training set
+    print(current_file['uri'])
+    #keys: uri, database
+    features = precomputed(current_file)   # load precomputed features
+
+    #get face landmarks file containing  facetrack segments
+    landmarks_file_name = train_dir + current_file['uri']+'.track.txt'
+    face_landmarks_numpy_array =  pd.read_csv(landmarks_file_name, sep=" ", header=None)
+    face_landmarks_numpy_array.columns =  ["time", "id", "left", "top","right","bottom","state"]
+
+    group_time_by_id = face_landmarks_numpy_array[['time', 'id']].groupby('id', as_index=False)
+
+    #extract only relevant facetracks
+    # Extract all Y numpy arrays ( all facetrack files) relevant to that file only
+    y_numpy_arrays_names = glob(y_labels_dir + current_file['uri'] + "*.Y.npy")
+
+    # get the list of facetracks ids
+    y_numpy_arrays = list(map(lambda y: y.split("/")[-1].split(".")[1], y_numpy_arrays_names))
+    #print(y_numpy_arrays)
+
+    for index in range(len(y_numpy_arrays)):
+        id = int(y_numpy_arrays[index])
+        #print(id)
+        min = group_time_by_id.min().ix[id]['time'].min()
+        max = group_time_by_id.min().ix[id]['time'].max()
+
+        Xa = features.crop(Segment(min, max))   # obtain features as numpy array for given temporal segment
+        Yv = np.load(y_labels_dir + current_file['uri'] + "." + str(id) + ".Y.npy")
+
+        np.save(output_dir_train + current_file['uri'] + '.' + str(id) + '.Xa.npy', Xa)
+        np.save(output_dir_train + current_file['uri']  + '.' + str(id) + '.Y.npy', Yv)
+
+
+def create_uem(input_name,output_name):
+    train_video_names = pd.read_csv(input_name, sep=" ", header=None)
+    train_video_names.columns = ["name"]
+    train_video_names["num1"]=0
+    train_video_names["num2"]=0
+    train_video_names["num3"]=0
+
+    train_video_names.to_csv(output_name, sep=" ", header=False, index_label=False, index=False)
+
+#create_uem('/vol/work1/dyab/training_set/train_video_list','plumcot.trn.uem')
+#create_uem('/vol/work1/dyab/development_set/dev0_video_list','plumcot.dev.uem')
+#create_uem('/vol/work1/dyab/test_set/test_video_list','plumcot.tst.uem')

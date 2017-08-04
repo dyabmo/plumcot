97deec5d5be31f99e2006187a99c787ae0695c21
dyabmo <dyabmo@github.com>
2017-08-04 18:08:37 +0200

    Evaluate LSTM trained with face and audio features.

--- lstm_evaluation.py
+++ lstm_evaluation.py
@@ -8,22 +8,55 @@ from glob import glob
 import os.path
 import time
 import sys
-
-#TODO: Use generator from script_lstm and script_audio_baseline_lstm
-#TODO: Apply on all WIGHTS_DIRs
+from script_lstm import get_generator
 
 BATCH_SIZE = 32
+REMOVE_LCP_TOPQUESTIONS=True
+USE_FACE=True
 WEIGHTS_DIR = '/vol/work1/dyab/training_models/bredin/one_out_of_ten_no_LCP_TopQuestions_face_audio'
 
+DEV_NUMPY_PATH_AUDIO = "/vol/work1/dyab/development_set/numpy_arrays_cluster_old_audio"
+DEV_NUMPY_PATH='/vol/work1/dyab/development_set/numpy_arrays_cluster_old_landmarks'
+
+TEST_NUMPY_PATH_AUDIO = "/vol/work1/dyab/test_set/numpy_arrays_audio"
+TEST_NUMPY_PATH='/vol/work1/dyab/test_set/numpy_arrays_landmarks'
+
+if USE_FACE:
+    DEV_NUMPY_PATH = '/vol/work1/dyab/development_set/numpy_arrays_cluster_old_landmarks_face'
+    TEST_NUMPY_PATH = '/vol/work1/dyab/test_set/numpy_arrays_landmarks_face'
+
+X_PATHS_DEV_AUDIO = sorted(glob(DEV_NUMPY_PATH_AUDIO + '/*.Xa.npy'))
+X_PATHS_DEV = sorted(glob(DEV_NUMPY_PATH + '/*.XLandmarks.npy'))
+Y_PATHS_DEV = sorted(glob(DEV_NUMPY_PATH + '/*.Y.npy'))
+
+#make sure audio and video files are the same
+X_PATHS_DEV = [f for f in X_PATHS_DEV if "LCP_EntreLesLignes_2011-04-05_025900" not in f]
+Y_PATHS_DEV = [f for f in Y_PATHS_DEV if "LCP_EntreLesLignes_2011-04-05_025900" not in f]
+
+X_PATHS_TEST_AUDIO = sorted(glob(TEST_NUMPY_PATH_AUDIO + '/*.Xa.npy'))
+X_PATHS_TEST = sorted(glob(TEST_NUMPY_PATH + '/*.XLandmarks.npy'))
+Y_PATHS_TEST = sorted(glob(TEST_NUMPY_PATH + '/*.Y.npy'))
+
+# If you want to remove LCP videos.
+if REMOVE_LCP_TOPQUESTIONS:
+    X_PATHS_DEV = [f for f in X_PATHS_DEV if "LCP_TopQuestions" not in f ]
+    Y_PATHS_DEV = [f for f in Y_PATHS_DEV if "LCP_TopQuestions" not in f ]
+    X_PATHS_TEST = [f for f in X_PATHS_TEST if "LCP_TopQuestions" not in f]
+    Y_PATHS_TEST = [f for f in Y_PATHS_TEST if "LCP_TopQuestions" not in f]
+
 #Select best model/epoch on dev set according to AUC curve
-dev_models = np.loadtxt(WEIGHTS_DIR+"/list_dev")
+
+with open(WEIGHTS_DIR+"/list_dev", "r") as f:
+    dev_models = f.readline().split(',')
+
+#print(dev_models)
 best_model = np.argmax(dev_models)
+print(best_model)
 model_h5 = WEIGHTS_DIR + '/{epoch:04d}.h5'.format(epoch=best_model)
 model = load_model(model_h5)
 f = open(WEIGHTS_DIR+"/scores",'w')
 f.write("Model: {:04d}\n".format(best_model))
 f.flush()
-
 
 #Apply the upcoming methods on that dev model
 
@@ -34,9 +67,9 @@ f.flush()
 score_matrix = np.zeros((3,3))
 i=0
 
-def validate(x_paths, y_paths, weights_dir):
+def validate_ev(x_paths, y_paths, weights_dir,x_paths_audio):
 
-    generator = get_generator(x_paths, y_paths, forever=False)
+    generator = get_generator(x_paths, y_paths, forever=False,x_paths_audio=x_paths_audio)
     signature = ({'type': 'ndarray'}, {'type': 'scalar'})
     batch_generator = batchify(generator, signature, batch_size=BATCH_SIZE)
 
@@ -51,21 +84,27 @@ def validate(x_paths, y_paths, weights_dir):
     #check y_pred values
     y_pred = np.vstack(Y_pred)
 
-    return Y_true,Y_pred
+    return y_true,y_pred
 
 #Beta represents the ratio between precision and recall weights in F-score calculation
 for beta in (1,0.1,10):
 
+    print(beta)
     #Apply dev set
-    y_true, y_pred = validate(x_paths_dev,y_paths_dev,WEIGHTS_DIR)
+    y_true, y_pred = validate_ev(X_PATHS_DEV,Y_PATHS_DEV,WEIGHTS_DIR,X_PATHS_DEV_AUDIO)
 
     max_fbeta_score=0
     argmax_theta=0
-    for theta in range(0.1,0.9,0.1):
+    for theta_index in range(1,10,1):
 
-        y_pred = y_pred > theta
-        score = fbeta_score(y_true, y_pred, beta=beta)
+        theta = theta_index/10.
+        print(theta)
 
+        #print(y_pred)
+
+        y_pred_cutoff = y_pred >= theta
+        score = fbeta_score(y_true, y_pred_cutoff, beta=beta)
+        print(score)
         if(score > max_fbeta_score):
             max_fbeta_score = score
             argmax_theta = theta
@@ -79,10 +118,10 @@ for beta in (1,0.1,10):
     # Apply the 3 thetas of that selected model on test set
     # Compare 3 f-scores of test set with dev set FOR EACH EXPERIMENT
     # If the scores are close, then generalization occurred.
-    Y_true, Y_pred = validate(x_paths_test,y_paths_test,WEIGHTS_DIR)
+    y_true_test, y_pred_test = validate_ev(X_PATHS_TEST,Y_PATHS_TEST,WEIGHTS_DIR,X_PATHS_TEST_AUDIO)
 
-    y_pred = y_pred > argmax_theta
-    test_score = fbeta_score(y_true, y_pred, beta=beta)
+    y_pred_test_cutoff = y_pred_test > argmax_theta
+    test_score = fbeta_score(y_true_test, y_pred_test_cutoff, beta=beta)
 
-    f.write("Beta: {}. Max_theta: {}. Development set F-score: {}.Test Set F-score\n".format(beta,argmax_theta,max_fbeta_score,test_score))
+    f.write("Beta: {}. Max_theta: {}. Development set F-score: {}.Test Set F-score: {}\n".format(beta,argmax_theta,max_fbeta_score,test_score))
     f.flush()
